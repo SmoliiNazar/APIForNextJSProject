8ed227a6640845acda55a9a94ef3f9d97c396c89
Nazar <smoliinazar3000@gmail.com>
2022-08-08 11:41:56 +0300

    new properties created

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -4,4 +4,13 @@ export class ProductModel {
     price: number;
     oldPrice: number;
     credit: number;
+    calculatedRating: number;
+    description: string;
+    advantages: string;
+    disAdvantages: string;
+    categories: string[];
+    tags: string;
+    characteristics: {
+        [key: string]: string;
+    };
 }
