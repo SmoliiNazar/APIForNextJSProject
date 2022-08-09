97f8be27e11768c8a461af5b596b6fbde715f4ca
Nazar <smoliinazar3000@gmail.com>
2022-08-09 18:58:51 +0300

    mongo connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule, 
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig
+    }), 
+    TopPageModule, 
+    ProductModule, 
+    ReviewModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
