15b442aeb1612f057d1fcf7724718a117b8f82dc
Nazar <smoliinazar3000@gmail.com>
2022-08-12 15:51:22 +0300

    top-page modified

--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -21,9 +21,25 @@ export class TopPageService {
     }
 
     async findByCategory(firstCategory: TopLevelCategory) {
-        return this.topPageModel.find({firstCategory}, {alias: 1, secondCategory: 1, title: 1}).exec();
+        return this.topPageModel.aggregate(
+            [
+                {
+                    $match: {
+                        firstCategory
+                    }
+                },
+                {
+                    $group: {
+                        _id: { secondCategory: '$secondCategory' },
+                        pages: {$push: { alias: '$alias', title: '$title' }}
+                    }
+                }
+            ]).exec();
     }
-
+    async findByText(text: string) {
+        return this.topPageModel.find({ $text: { $search: text, $caseSensitive: false }}).exec();
+    
+    }
     async updateById(id: string, dto: CreateTopPageDto) {
         return this.topPageModel.findByIdAndUpdate(id, dto, {new: true}).exec();
     }
@@ -31,6 +47,4 @@ export class TopPageService {
     async deleteById(id: string) {
         return this.topPageModel.findByIdAndRemove(id).exec();
     }
-
-
 }
